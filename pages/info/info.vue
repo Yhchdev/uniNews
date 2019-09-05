205084c54e94213b03661e9bf3b170f196d77e37
Yhchdev <630673943@qq.com>
2019-09-05 23:35:44 +0800

    uinapp demo实战 新闻系统

---
+++ pages/info/info.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<view class="title">{{title}}</view>
+		<view class="art-content">
+			<rich-text class="richText" :nodes="strings"></rich-text>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title:'',
+				strings:''
+			}
+		},
+		methods: {
+			onLoad(e){
+				//console.log(e);
+				uni.request({
+					url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' +e.newsid,
+					method: 'GET',
+					data: {},
+					success: res => {
+						this.title = res.data.title
+						this.strings = res.data.content
+						//console.log(res)
+					},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+			
+		}
+	}
+</script>
+
+<style>
+	.content{padding: 1em 3%; width: 94%; flex-wrap: wrap;}
+	.title{line-height: 2em; font-weight: 700 ;font-size: 38upx;}
+	.art-content{line-height: 2em;}
+
+</style>
